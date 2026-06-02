ba153c0a5fab64aaa8bb21eec2b4a2896d224910
scota <scota836@hsd.k12.or.us>
2026-06-02 09:10:57 -0700

    fuh nah, ts does not work at all
    i think i was too over my head thinking i could do a library, im just gonne combine strings and slice stuff and give cool names, im not going to do an external library, scary stuff

--- src/final_username_maker_checker/pyside6_test.py
+++ src/final_username_maker_checker/pyside6_test.py
@@ -1,5 +1,3 @@
-
-
 import sys
 from PySide6.QtWidgets import (
     QApplication,
@@ -8,7 +6,7 @@ from PySide6.QtWidgets import (
     QMainWindow,
     QPushButton,
     QVBoxLayout,
-    QWidget,
+    QWidget,QHBoxLayout
   
 )
 from PySide6.QtGui import QFont
@@ -21,6 +19,8 @@ class MainWindow(QMainWindow):
         self.setContentsMargins(12, 12, 12, 12)
         self.resize(320, 240)
 
+        hbox = QHBoxLayout()
+
         layout = QVBoxLayout()
         title_label = QLabel("Enter a premade username if you have one:")
         explanatory_sentence = QLabel("This app is for generating usernames and checking to see if theyve been created already or not.")
@@ -31,33 +31,42 @@ class MainWindow(QMainWindow):
         submit_button = QPushButton("greet me!")
         submit_button.clicked.connect(self.get_input)
         # TODO: add a label to greet user
-        self.instructions = "Enter your name and click the button to be greeted!"
+        
+        self.instructions = "Enter your name and click the button, then, enter your job in the box to the right."
         self.output_label = QLabel(self.instructions)
-        self.job_inputs = QLineEdit(placeholderText="enter your jobb")
-        submit_button_job = QPushButton("job!!")
+        self.job_inputs = QLineEdit(placeholderText="enter your first name")
+        self.name_datas = QLineEdit(placeholderText= "Type your name here...")
+        
+        submit_button_job = QPushButton("username")
         submit_button_job.clicked.connect(self.job_input)
 
-       
+        submit_button_data = QPushButton("Your name")
+        submit_button_data.clicked.connect(self.name_data)
+        
         # add widgets & layouts to main layout
         layout.addWidget(explanatory_sentence)
         layout.addWidget(title_label)
         layout.addWidget(self.name_input)
         layout.addWidget(submit_button)
         layout.addWidget(self.output_label)
-        layout.addWidget(self.job_inputs)
-        layout.addWidget(submit_button_job)
+        hbox.addWidget(self.name_datas) 
+        hbox.addWidget(submit_button_data)
+        hbox.addWidget(self.job_inputs)
+        hbox.addWidget(submit_button_job)
         # [OPTIONAL] Add a stretch to move everything up
         layout.addStretch()
 
         widget = QWidget()
         widget.setLayout(layout)
+        widget.setLayout(hbox)
 
         # Set the central widget of the Window.
         self.setCentralWidget(widget)
-    
+
+
     def get_input(self):
         output = "Hello, "
-        name = self.name_input.text()
+        name = self.get_input.text()
         if not name:
             output = "friend" + "Give a real name next time, you didn't this time!" 
             output += "your name is empty!"
@@ -65,12 +74,22 @@ class MainWindow(QMainWindow):
             output = f"You've entered: {name} as your username." 
         self.output_label.setText(output)
 
+    def name_data(self):
+        output = "Hello, "
+        name_d = self.name_datas.text()
+        if not name_d:
+           output = f"g"
+           output  += "n"
+        else:
+            output = f"you entered, {name_d} as your job"
+        self.output_label.setText(output)
+    
     def job_input(self):
         output = "Hello, "
         job = self.job_inputs.text()
         if not job:
-           output = f"give a real job"
-           output  += "enter a job!"
+           output = f"g"
+           output  += "n"
         else:
             output = f"you entered, {job} as your job"
         self.output_label.setText(output)
